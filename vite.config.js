8942c291c49fae34e018e4d8b99f5deac80ecb9b
merosman91 <merghanigasimosman@gmail.com>
2025-12-17 17:14:21 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
   build: {
-    outDir: 'dist',
-    sourcemap: true,
-  }, 
-    resolve: {
-      extensions: ['.js', '.jsx', '.json']
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom']
+        }
+      }
+    }
   }
 })
